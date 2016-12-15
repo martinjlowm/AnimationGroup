b74782d8c3d0aedd909a06d3391dea0899b462f7
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2016-12-15 23:37:23 +0100

    Reworks test setup

--- tests.lua
+++ tests.lua
@@ -6,25 +6,30 @@ if not AG then return end
 local tests = {}
 
 local frame = CreateFrame('Frame')
-frame:SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
-frame:SetWidth(128)
-frame:SetHeight(128)
-
-local tex = frame:CreateTexture('ARTWORK')
-tex:SetAllPoints(frame)
-tex:SetBlendMode('ADD')
-tex:SetTexture('Interface\\Icons\\Ability_Warrior_WarCry')
-frame:Show()
-
+local tex
 local ag
 
 local Setup = function()
+    frame = CreateFrame('Frame')
+    frame:SetWidth(128)
+    frame:SetHeight(128)
+    frame:SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
+
+    tex = frame:CreateTexture('ARTWORK')
+    tex:SetAllPoints(frame)
+    tex:SetBlendMode('ADD')
+    frame:Show()
+
     ag = frame:CreateAnimationGroup()
     ag:SetLooping('BOUNCE')
 end
-table.insert(tests, Setup)
 
 local TranslationScaleRotationAlphaAnimation = function()
+    frame:ClearAllPoints()
+    frame:SetPoint('CENTER', UIParent, 'CENTER', 200, 0)
+
+    tex:SetTexture('Interface\\Icons\\Ability_Warrior_WarCry')
+
     local translation = ag:CreateAnimation('Translation')
     translation:SetOffset(100, 100)
     translation:SetDuration(1)
@@ -36,7 +41,7 @@ local TranslationScaleRotationAlphaAnimation = function()
     scale:SetSmoothing('OUT')
 
     local rotation = ag:CreateAnimation('Rotation')
-    rotation:SetDegrees(0)
+    rotation:SetDegrees(90)
     rotation:SetDuration(2)
     rotation:SetSmoothing('INOUT')
 
@@ -47,36 +52,59 @@ local TranslationScaleRotationAlphaAnimation = function()
 
     ag:Play()
 end
+table.insert(tests, Setup)
 table.insert(tests, TranslationScaleRotationAlphaAnimation)
 
 local NewScaleAnimation = function()
-    local frame = CreateFrame('Frame')
-    frame:SetPoint('CENTER', UIParent, 'CENTER', -200, 0)
-    frame:SetWidth(64)
-    frame:SetHeight(64)
+    frame:ClearAllPoints()
+    frame:SetPoint('CENTER', UIParent, 'CENTER', 0, 250)
 
-    local tex = frame:CreateTexture('ARTWORK')
-    tex:SetAllPoints(frame)
-    tex:SetBlendMode('ADD')
     tex:SetTexture('Interface\\Icons\\INV_Misc_Pelt_Bear_Ruin_02')
-    frame:Show()
-
-    local ag = frame:CreateAnimationGroup()
-    ag:SetLooping('BOUNCE')
 
     local scale = ag:CreateAnimation('Scale')
-    scale:SetScale(1.5, 1.5)
+    scale:SetScale(-2, -2)
     scale:SetDuration(2.5)
     scale:SetSmoothing('OUT')
 
     ag:Play()
 end
+table.insert(tests, Setup)
 table.insert(tests, NewScaleAnimation)
+
+local OrderAnimations = function()
+    frame:ClearAllPoints()
+    frame:SetPoint('CENTER', UIParent, 'CENTER', -250, 0)
+
+    tex:SetTexture('Interface\\Icons\\Spell_Holy_InnerFire')
+
+    local scale = ag:CreateAnimation('Scale')
+    scale:SetScale(1.5, 1.5)
+    scale:SetDuration(1)
+    scale:SetSmoothing('OUT')
+    scale:SetOrder(0)
+
+    local rotation = ag:CreateAnimation('Rotation')
+    rotation:SetDegrees(180)
+    rotation:SetDuration(1)
+    rotation:SetSmoothing('OUT')
+    rotation:SetOrder(1)
+
+    local alpha = ag:CreateAnimation('Alpha')
+    alpha:SetChange(0.5)
+    alpha:SetDuration(1)
+    alpha:SetSmoothing('OUT')
+    alpha:SetOrder(2)
+
+    ag:Play()
+end
+table.insert(tests, Setup)
+table.insert(tests, OrderAnimations)
 
 local function OnEvent()
     for _, test in next, tests do
         test()
     end
+    tests = nil
 end
 
 frame:RegisterEvent('UPDATE_INSTANCE_INFO')
