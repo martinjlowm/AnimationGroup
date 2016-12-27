22f0c2e27155dffd708532d23dc333aff632b09e
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2016-12-27 15:24:09 +0100

    Adds MIT license and updates README.org

--- path.lua
+++ path.lua
@@ -1,3 +1,25 @@
+--[[
+    Copyright (c) 2016 Martin Jesper Low Madsen <martin@martinjlowm.dk>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+--]]
+
 if not LibStub then return end
 
 local AG = LibStub:GetLibrary('AnimationGroup-1.0')
@@ -6,6 +28,10 @@ if not AG then return end
 local Path = AG:New('Path', AG.Animation)
 Path.curve_type = nil
 Path.control_points = {}
+
+function Path:__Initialize()
+    assert(nil, 'Animation paths are not yet supported!')
+end
 
 -- Adds a new path control point.
 function Path:CreateControlPoint(name, template, order)
