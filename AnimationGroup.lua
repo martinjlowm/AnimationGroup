e162190284aefed32c0c4c587e04bfcb1275296e
LaYt <laytya@gmail.com>
2022-09-19 12:00:56 +0700

    Fix for error

--- AnimationGroup.lua
+++ AnimationGroup.lua
@@ -129,7 +129,7 @@ function AG:LoadProperties(group)
 
     local point = group.properties.point
         -- old implementation some times fires error =)
-    if point and type(point) == "table" then  
+    if point and type(point) == "table" and table.getn(point) > 0 then  
 	      local point, relative_to, relative_point, offset_x, offset_y = unpack(point)
 	      relative_to = type(relative_to) ~= "function" and relative_to or nil 
         group.parent:SetPoint(point, relative_to, relative_point, offset_x, offset_y)
